6f7ec29cddb08e8a1ad3ac81058eaeabd495d745
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-05-20 15:25:53 +0700

    Add home, register, and welcome routes

--- pertemuan-11/routes/web.php
+++ pertemuan-11/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/register', [AuthController::class, 'register']);
+Route::get('/welcome', [AuthController::class, 'welcome']);
