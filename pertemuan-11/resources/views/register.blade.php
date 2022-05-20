cf65bc5fab0e9cbe2c7fbb7e9ef83fbf6ccf816e
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-05-20 15:23:48 +0700

    Create register view

---
+++ pertemuan-11/resources/views/register.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register Page</title>
+</head>
+<body>
+    <form action="/welcome">
+        <input type="text" name="firstName" placeholder="Nama Depan">
+        <input type="text" name="lastName" placeholder="Nama Belakang">
+        <input type="submit" value="Kirim">
+    </form>
+</body>
+</html>
